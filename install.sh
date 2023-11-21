df6121fbbb0417e0bdc42c4f4564b3e7e33b110c
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-11-21 07:07:50 +0000

    brewをオプションから存在確認して実行に変更

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply mouse484 --promptBool brew=false,asdf=false
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply mouse484 --promptBool asdf=false
