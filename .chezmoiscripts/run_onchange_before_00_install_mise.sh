02e4f826d46ecb117a230ea8b18ad42ceda2274b
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-04-30 18:24:08 +0900

    Fix formatting of curl command in mise installation script

--- .chezmoiscripts/run_onchange_before_00_install_mise.sh
+++ .chezmoiscripts/run_onchange_before_00_install_mise.sh
@@ -5,7 +5,7 @@ echo "Setup mise"
 if [ ! $(command -v mise) ]; then
     echo "Install aqua"
     cd ~/.cache
-    curl -o mise-installer  https://mise.run
+    curl -o mise-installer https://mise.run
     chmod +x mise-installer
     ./mise-installer
     rm -f mise-installer
