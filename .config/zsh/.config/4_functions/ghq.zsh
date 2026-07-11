d87b8ca87be26ab79026121a16da01b6e760dfc9
mouse <me@mousedev.page>
2026-07-11 17:07:50 +0900

    fix: use command prefix for ghq calls in function to ensure proper execution

--- .config/zsh/.config/4_functions/ghq.zsh
+++ .config/zsh/.config/4_functions/ghq.zsh
@@ -3,9 +3,9 @@ function ghq() {
 
   case $subcommand in
   cd)
-    local src=$(ghq list | fzf --preview "bat --color=always $(ghq root)/{}/README.md")
+    local src=$(command ghq list | fzf --preview "bat --color=always $(command ghq root)/{}/README.md")
     if [[ -n $src ]]; then
-      local path=$(ghq list -p "$src")
+      local path=$(command ghq list -p "$src")
       if [[ -d $path ]]; then
         cd "$path" || return 1
       fi
